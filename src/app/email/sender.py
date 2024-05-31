9c25000e59137206c545e98227f8757d5f132252
Abraham Montoya <montoyaobeso@gmail.com>
2024-05-31 16:07:44 -0700

    feat: get aws secrets

--- src/app/email/sender.py
+++ src/app/email/sender.py
@@ -2,15 +2,28 @@ import os
 
 import sendgrid
 from sendgrid.helpers.mail import Mail, Email, To, Content
+from src.app.aws.secrets import get_secret
+
+
+def set_credentials_from_aws_secrets():
+    secret = get_secret("stori-sendgrid")
+    os.environ["SENDGRID_SENDER_EMAIL"] = secret["SENDGRID_SENDER_EMAIL"]
+    os.environ["SENDGRID_API_KEY"] = secret["SENDGRID_API_KEY"]
 
 
 def send_email(
     subject: str,
-    sender: str,
     recipient: str,
     body_content: str,
 ) -> None:
 
+    if (
+        "SENDGRID_SENDER_EMAIL" not in os.environ
+        or "SENDGRID_API_KEY" not in os.environ
+    ):
+        set_credentials_from_aws_secrets()
+
+    sender = os.environ["SENDGRID_SENDER_EMAIL"]
     sg = sendgrid.SendGridAPIClient(api_key=os.environ["SENDGRID_API_KEY"])
 
     mail = Mail(
