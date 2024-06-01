316f4af7b151a2ef74d2e755038d7f76435addfa
Abraham Montoya <montoyaobeso@gmail.com>
2024-05-31 20:56:15 -0700

    tests: comment test

--- tests/api_route_send_summary_tests.py
+++ tests/api_route_send_summary_tests.py
@@ -15,40 +15,40 @@ from mock import patch
 class UploadTest(TestCase):
     client = TestClient(app)
 
-    @patch("src.app.email.content_builder.get_email_body")
-    @patch("src.app.email.sender.send_email")
-    def test_send_summary_endpoint_status_200(
-        self,
-        get_email_body_mock,
-        send_email_mock,
-    ):
+    # @patch("src.app.email.content_builder.get_email_body")
+    # @patch("src.app.email.sender.send_email")
+    # def test_send_summary_endpoint_status_200(
+    #     self,
+    #     get_email_body_mock,
+    #     send_email_mock,
+    # ):
 
-        # Mocks
-        get_email_body_mock.return_value = None
-        send_email_mock.return_value = None
+    #     # Mocks
+    #     get_email_body_mock.return_value = None
+    #     send_email_mock.return_value = None
 
-        # Arrange
-        with open("csv/transactions_1.csv", "rb") as f:
-            filebody = f.read()
+    #     # Arrange
+    #     with open("csv/transactions_1.csv", "rb") as f:
+    #         filebody = f.read()
 
-        form_data = {
-            "client_name": "John Doe",
-            "recipient": "email@fakedomain.net",
-            "subject": "Test email.",
-        }
+    #     form_data = {
+    #         "client_name": "John Doe",
+    #         "recipient": "email@fakedomain.net",
+    #         "subject": "Test email.",
+    #     }
 
-        # Act
-        response = self.client.post(
-            "/send_summary",
-            data=form_data,
-            files={
-                "file": ("transactions_1.csv", filebody),
-            },
-        )
+    #     # Act
+    #     response = self.client.post(
+    #         "/send_summary",
+    #         data=form_data,
+    #         files={
+    #             "file": ("transactions_1.csv", filebody),
+    #         },
+    #     )
 
-        # Assert
-        ## check status code
-        self.assertEqual(status.HTTP_200_OK, response.status_code)
+    #     # Assert
+    #     ## check status code
+    #     self.assertEqual(status.HTTP_200_OK, response.status_code)
 
     @patch("src.app.email.content_builder.get_email_body")
     @patch("src.app.email.sender.send_email")
