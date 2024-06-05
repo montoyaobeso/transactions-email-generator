13f91e07469e7dfee421e942be301dde082c3e72
Abraham Montoya <montoyaobeso@gmail.com>
2024-06-05 14:01:26 -0700

    feat: restore api script

--- src/api/main.py
+++ src/api/main.py
@@ -4,47 +4,37 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# from src.api.routers import root
 
-# from src.api.routers import presiged_url
-# from src.api.routers import load_transactions
-# from src.api.routers import load_transactions_s3
-# from src.api.routers import account
-# from src.api.routers import transaction
-# from src.api.routers import send_balance
+from src.app.db import models
+from src.app.db.database import engine
 
+models.Base.metadata.create_all(bind=engine)
 
-# app = FastAPI(
-#     title="Transactions Email Generator",
-#     description="Generate a transactions summary email.",
-#     version="0.0.1",
-#     contact={
-#         "name": "Abraham Montoya",
-#         "email": "montoyaobeso@gmail.com",
-#     },
-# )
-
-# app.include_router(root.router)
-# app.include_router(account.router)
-# app.include_router(transaction.router)
-# app.include_router(presiged_url.router)
-# app.include_router(load_transactions.router)
-# app.include_router(load_transactions_s3.router)
-# app.include_router(send_balance.router)
-
-# handler = Mangum(app)
+from src.api.routers import root
+from src.api.routers import presiged_url
+from src.api.routers import load_transactions
+from src.api.routers import load_transactions_s3
+from src.api.routers import account
+from src.api.routers import transaction
+from src.api.routers import send_balance
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Transactions Email Generator",
+    description="Generate a transactions summary email.",
+    version="0.0.1",
+    contact={
+        "name": "Abraham Montoya",
+        "email": "montoyaobeso@gmail.com",
+    },
+)
 
-
-@app.get("/")
-async def root():
-    from src.app.db import models
-    from src.app.db.database import engine
-
-    models.Base.metadata.create_all(bind=engine)
-    return {"message": "Hello World"}
-
+app.include_router(root.router)
+app.include_router(account.router)
+app.include_router(transaction.router)
+app.include_router(presiged_url.router)
+app.include_router(load_transactions.router)
+app.include_router(load_transactions_s3.router)
+app.include_router(send_balance.router)
 
 handler = Mangum(app)
