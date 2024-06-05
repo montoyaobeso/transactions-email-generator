bc7969c4fb7c7ade1434b827f220cbf23c180bc8
Abraham Montoya <montoyaobeso@gmail.com>
2024-06-05 16:59:23 -0700

    fix: fix typo in env var

--- src/app/db/database.py
+++ src/app/db/database.py
@@ -10,10 +10,10 @@ if os.environ["STAGE"] != "local":
     db_credentials = get_secret("stori-database-credentials")
     os.environ["POSTGRES_USER"] = db_credentials["username"]
     os.environ["POSTGRES_PASSWORD"] = db_credentials["password"]
-    os.environ["POSTRES_HOST"] = db_credentials["host"]
+    os.environ["POSTGRES_HOST"] = db_credentials["host"]
     os.environ["POSTGRES_DB"] = db_credentials["database"]
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTRES_HOST']}/{os.environ['POSTGRES_DB']}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
