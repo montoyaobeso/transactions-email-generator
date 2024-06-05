fec58eb2860ea3fa85bc0b548285d680388c5275
Abraham Montoya <montoyaobeso@gmail.com>
2024-06-05 11:39:12 -0700

    feat: remove function to set credentials

--- src/app/db/database.py
+++ src/app/db/database.py
@@ -11,12 +11,14 @@ logger.setLevel(logging.INFO)
 
 if os.environ["STAGE"] != "local":
     db_credentials = get_secret("stori-database-credentials")
-    logger.info(type(db_credentials))
-    logger.info(db_credentials.keys())
+    logger.info("Credentials response type:", type(db_credentials))
+    logger.info("Credentials keys: ", db_credentials.keys())
     os.environ["POSTGRES_USER"] = db_credentials["username"]
     os.environ["POSTGRES_PASSWORD"] = db_credentials["password"]
     os.environ["POSTRES_HOST"] = db_credentials["host"]
     os.environ["POSTRES_DB"] = db_credentials["database"]
+else:
+    logger.info("Executing in local enviroment.")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTRES_HOST']}/{os.environ['POSTGRES_DB']}"
 
