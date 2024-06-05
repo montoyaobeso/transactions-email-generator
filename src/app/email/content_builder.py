20c03ab30d74ef228f96c59c89eecaf4766b5481
Abraham Montoya <montoyaobeso@gmail.com>
2024-06-05 09:01:46 -0700

    feat: add transactions history section

--- src/app/email/content_builder.py
+++ src/app/email/content_builder.py
@@ -9,14 +9,14 @@ class EmailBodyBuilder:
         total_balance,
         avg_credit_amount,
         avg_debit_amount,
-        transactions_per_month,
+        transactions_history,
     ) -> None:
 
         self.client_name = client_name
         self.total_balance = total_balance
         self.avg_credit_amount = avg_credit_amount
         self.avg_debit_amount = avg_debit_amount
-        self.transactions_per_month = transactions_per_month
+        self.transactions_history = transactions_history
 
     def get_email_body(self) -> str:
 
@@ -26,17 +26,26 @@ class EmailBodyBuilder:
 
         template = env.get_template("./email_body.html")
 
+        transactions_summary = []
+        for year, txns_per_month in self.transactions_history.items():
+            transactions_summary.append(f'<li style="Margin:0">{year}:</li><ul>')
+            transactions_summary += (
+                "".join(
+                    [
+                        f'<li style="Margin:0">{month}: {count:,.0f}</li>'
+                        for month, count in txns_per_month
+                    ]
+                ),
+            )
+
+            transactions_summary.append("</ul>")
+
         fields = {
             "client_name": self.client_name.title(),
             "total_balance": f"${self.total_balance:,.2f}",
             "avg_debit_amount": f"${self.avg_debit_amount:,.2f}",
             "avg_credit_amount": f"${self.avg_credit_amount:,.2f}",
-            "transactions_per_month": "".join(
-                [
-                    f'<li style="Margin:0">{month}: {count:,.0f}</li>'
-                    for month, count in self.transactions_per_month
-                ]
-            ),
+            "transactions_history": "".join(transactions_summary),
         }
 
         return template.render(fields)
