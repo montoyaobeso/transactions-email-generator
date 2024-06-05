99811881492a86dd4c5a4c66a17757b105b65842
Abraham Montoya <montoyaobeso@gmail.com>
2024-06-05 11:30:32 -0700

    ci: test lambda init

--- src/api/main.py
+++ src/api/main.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 from src.api.routers import root
 
-from src.api.routers import presiged_url
-from src.api.routers import load_transactions
-from src.api.routers import load_transactions_s3
-from src.api.routers import account
-from src.api.routers import transaction
-from src.api.routers import send_balance
+# from src.api.routers import presiged_url
+# from src.api.routers import load_transactions
+# from src.api.routers import load_transactions_s3
+# from src.api.routers import account
+# from src.api.routers import transaction
+# from src.api.routers import send_balance
 
 from src.app.db import models
 from src.app.db.database import engine
@@ -19,22 +19,35 @@ from src.app.db.database import engine
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(
-    title="Transactions Email Generator",
-    description="Generate a transactions summary email.",
-    version="0.0.1",
-    contact={
-        "name": "Abraham Montoya",
-        "email": "montoyaobeso@gmail.com",
-    },
-)
+# app = FastAPI(
+#     title="Transactions Email Generator",
+#     description="Generate a transactions summary email.",
+#     version="0.0.1",
+#     contact={
+#         "name": "Abraham Montoya",
+#         "email": "montoyaobeso@gmail.com",
+#     },
+# )
 
-app.include_router(root.router)
-app.include_router(account.router)
-app.include_router(transaction.router)
-app.include_router(presiged_url.router)
-app.include_router(load_transactions.router)
-app.include_router(load_transactions_s3.router)
-app.include_router(send_balance.router)
+# app.include_router(root.router)
+# app.include_router(account.router)
+# app.include_router(transaction.router)
+# app.include_router(presiged_url.router)
+# app.include_router(load_transactions.router)
+# app.include_router(load_transactions_s3.router)
+# app.include_router(send_balance.router)
+
+# handler = Mangum(app)
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 
 handler = Mangum(app)
