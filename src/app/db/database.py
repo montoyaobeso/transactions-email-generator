5f280d5f468b610220e941955d573896cfa69231
Abraham Montoya <montoyaobeso@gmail.com>
2024-06-10 12:56:02 -0700

    feat: update get env condition

--- src/app/db/database.py
+++ src/app/db/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from src.app.aws.secrets import get_secret
 
-if os.environ["STAGE"] != "local":
+if os.environ["STAGE"] != "local" and "POSTGRES_HOST" not in os.environ:
     db_credentials = get_secret("stori-database-credentials")
     os.environ["POSTGRES_USER"] = db_credentials["username"]
     os.environ["POSTGRES_PASSWORD"] = db_credentials["password"]
