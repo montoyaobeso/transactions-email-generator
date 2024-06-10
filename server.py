b1cfa791df2b66c8d8a67b9175e9b9a7d25acf87
Abraham Montoya <montoyaobeso@gmail.com>
2024-06-10 12:55:17 -0700

    chore: print env keys

--- server.py
+++ server.py
@@ -37,10 +37,11 @@ app.include_router(root.router)
 
 if __name__ == "__main__":
     print("Starting webserver...")
+    print(os.environ.keys())
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8080)),
+        port=int(os.getenv("SERVER_PORT", 8080)),
         log_level=os.getenv("LOG_LEVEL", "info"),
         proxy_headers=True,
     )
