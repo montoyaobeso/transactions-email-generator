3ea090912158bc9779afec929b3ee0446705d8e2
Abraham Montoya <montoyaobeso@gmail.com>
2024-06-09 22:35:02 -0700

    ci: deploy api

--- server.py
+++ server.py
@@ -1,25 +1,44 @@
-import fastapi, uvicorn
-from fastapi.responses import JSONResponse
 import os
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
 
-api = fastapi.FastAPI()
-
-
-@api.get("/")
-async def root():
-    """Get a welcome messge from the API."""
-    return JSONResponse(
-        content={"message": "Welcome to transactions email generator API."},
-        status_code=200,
-    )
+load_dotenv()
 
 
-...
+from src.api.routers import (
+    account,
+    load_transactions,
+    load_transactions_s3,
+    presiged_url,
+    root,
+    send_balance,
+    transaction,
+)
+
+app = FastAPI(
+    title="Transactions Email Generator",
+    description="Manage accounts, transactions and send balances to registered email.",
+    version="0.0.1",
+    contact={
+        "name": "Abraham Montoya",
+        "email": "montoyaobeso@gmail.com",
+    },
+)
+
+app.include_router(root.router)
+app.include_router(account.router)
+app.include_router(transaction.router)
+app.include_router(presiged_url.router)
+app.include_router(load_transactions.router)
+app.include_router(load_transactions_s3.router)
+app.include_router(send_balance.router)
+
 
 if __name__ == "__main__":
     print("Starting webserver...")
     uvicorn.run(
-        api,
+        app,
         host="0.0.0.0",
         port=int(os.getenv("PORT", 8080)),
         log_level=os.getenv("LOG_LEVEL", "info"),
